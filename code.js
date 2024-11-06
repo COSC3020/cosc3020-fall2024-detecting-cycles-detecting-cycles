c708568b623fdeadcc336f0225c7812c2e64be4c
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-11-06 15:31:18 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,28 @@
-function hasCycle(graph) {
-    return false;
+// function hasCycle(graph) {
+//     return false;
+// }
+
+
+function depthFirstSearch(graph, startNode, targetNode) { // note that graph will be an adjacency list
+    let visited = [] 
+    let path = []
+    
+    function depthFirstHelp(node){
+        if (visited.includes(node)) return false;
+        if (node === targetNode) {
+          path.push(node);
+          return true;
+        }
+        visited.push(node);
+        path.push(node);
+        for(let i = 0; i < graph[node].length; i++){
+            if(depthFirstHelp(graph[node][i]))
+              return true;
+        }
+        path.pop(); // pop it because the target wasn't found on it's path
+        return false;
+    }
+    
+    depthFirstHelp(startNode);
+    return path;
 }
